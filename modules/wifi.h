a4ee1bfbb386a8267b65f11c6216cb31873ce4ae
Farhan P <farhan.abuhazan@gmail.com>
2018-12-09 09:56:24 +0700

    new wifi & lcd status

--- modules/wifi.h
+++ modules/wifi.h
@@ -7,7 +7,7 @@
 
 const char *ssid = "vmnnetwork";
 const char *password = "raspberry";
-
+const char *host = "192.168.4.1";
 extern TaskManager taskManager;
 
 class VmnClient : public Task
@@ -24,8 +24,8 @@ class VmnClient : public Task
         Serial.println(ssid);
 
         WiFi.begin(ssid, password);
-        
-        lcd.setCursor(0,0);
+
+        lcd.setCursor(0, 0);
         lcd.print("Connecting...");
         while (WiFi.status() != WL_CONNECTED)
         {
@@ -38,7 +38,7 @@ class VmnClient : public Task
         Serial.println("IP address: ");
         Serial.println(WiFi.localIP());
 
-        lcd.setCursor(0,0);
+        lcd.setCursor(0, 0);
         lcd.print("WiFi Connected");
     }
 
@@ -64,80 +64,62 @@ class VmnClient : public Task
 
         if ((WiFi.status() == WL_CONNECTED))
         { //Check the current connection status
+            Serial.print("connecting to ");
+            Serial.println(host);
 
-            float ec = ECSensor::instance()->GetEC();
-            float vol = LoadCell::instance()->getVal();
-
-            Serial.println(ec);
-            Serial.println(station);
-            Serial.println(vol);
-            // String url = "http://192.168.4.1/vmndata";
-            String url = "http://192.168.4.1/vmndata?" + String(station) + "," + String(ec) + "," + String(vol);
-            // String url = "http://192.168.4.1/vmndata?st=" + String(station) + "&ec=" + String(ec) + "&vol=" + String(vol);
-            Serial.println(url);
-            HTTPClient http;
-
-            http.begin(url); //Specify the URL
-            int httpCode = http.GET();                                        //Make the request
-
-            if (httpCode > 0)
-            { //Check for the returning code
-                String payload = http.getString();
-                Serial.println(httpCode);
-                Serial.println(payload);
-            }
-
-            else
+            WiFiClient client;
+            const int httpPort = 80;
+            if (!client.connect(host, httpPort))
             {
                 lcd.setCursor(0,1);
-                lcd.print("Error HTTP");
-                Serial.println("Error on HTTP request");
+                lcd.print("HTTP FAILED");
+                Serial.println("connection failed");
+                return;
             }
+            float ec = ECSensor::instance()->GetEC();
+            float vol = LoadCell::instance()->getVal();
+            String url = "/vmndata?" + String(station) + "," + String(ec) + "," + String(vol);
 
-            http.end(); //Free the resources
-        }
-
-        /*Serial.print("connecting to ");
-        Serial.println(host);
-
-        WiFiClient client;
-        const int httpPort = 3000;
-        if (!client.connect(host, httpPort))
-        {
-            Serial.println("connection failed");
-            return;
-        }
-        float ec = ECSensor::instance()->GetEC();
-        float vol = LoadCell::instance()->getVal();
-        String url = "/vmndata?" + String(STA) + "," + String(ec) + "," + String(vol);
-
-        // This will send the request to the server
-        if (state == 0)
-        {
-            currentTime += delta_time;
-            if (currentTime > 1000)
+            
+            String lcdStr = String(station) + " val:"+String(ec) + "/" + String(vol);
+            lcd.setCursor(0,1);
+            lcd.print(lcdStr);
+            // This will send the request to the server
+            if (state == 0)
             {
-                Serial.print("Requesting URL: ");
-                Serial.println(url);
-                client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                             "Host: " + host + "\r\n" +
-                             "Connection: close\r\n\r\n");
-                state = 1;
-                currentTime = 0;
+                currentTime += delta_time;
+                if (currentTime > 2000)
+                {
+                    Serial.print("Requesting URL: ");
+                    Serial.println(url);
+                    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                                 "Host: " + host + "\r\n" +
+                                 "Connection: close\r\n\r\n");
+                    state = 1;
+                    currentTime = 0;
+                }
             }
-        }
-        else if (state == 1)
-        {
-            currentTime += delta_time;
-            if (currentTime > 1000)
+            else if (state == 1)
             {
-                 Serial.print("close connection");
-                state = 0;
-                currentTime = 0;
-                client.stop();
+                currentTime += delta_time;
+                if (currentTime > 3000)
+                {
+                    Serial.print("close connection");
+                    state = 0;
+                    currentTime = 0;
+                    client.stop();
+                }
+
+                if(client.available()){
+                    Serial.print("Recieve data");
+                    client.stop();
+                    state = 0;
+                    currentTime = 0;
+                    client.stop();
+                }
             }
+            // Read all the lines of the reply from server and print them to Serial*/
         }
-        // Read all the lines of the reply from server and print them to Serial*/
     }
 };
 VmnClient *VmnClient::s_instance = 0;
